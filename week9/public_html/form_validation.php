9a2a2e4ad17af5b681efefe25e8160b0564a3f78
Jasmine Tolentino <jatolentino3@student.apc.edu.ph>
2023-02-08 08:00:47 +0800

    minimal fix

--- week9/public_html/form_validation.php
+++ week9/public_html/form_validation.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=<?php
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
 // define variables and set to empty values
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
@@ -95,9 +99,5 @@ echo $comment;
 echo "<br>";
 echo $gender;
 ?>
-    <title>Document</title>
-</head>
-<body>
-    
 </body>
 </html>
